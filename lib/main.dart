1b95ea66a0462e0a80fba2023d3319514c722bc2
Celeste Vagilidad <c.vagilidad.522785@umindanao.edu.ph>
2023-10-01 19:17:40 +0800

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:ezmoney/models/appstyle.dart';
+import 'package:page_transition/page_transition.dart';
 import '/startpage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -18,13 +21,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromRGBO(155, 128, 231, 1)),
-          useMaterial3: true,
-        ),
-        home: StartPage());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(155, 128, 231, 1)),
+        useMaterial3: true,
+      ),
+      home: AnimatedSplashScreen(
+        splash: 'images/EZLOGO 2.png',
+        duration: 3500,
+        pageTransitionType: PageTransitionType.bottomToTop,
+        nextScreen: StartPage(),
+        splashTransition: SplashTransition.scaleTransition,
+        backgroundColor: primaryColor(1),
+      ),
+    );
   }
 }
