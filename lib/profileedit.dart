4960d28cd4bd3c5c605b104dad79dba81c69c0eb
Celeste Vagilidad <c.vagilidad.522785@umindanao.edu.ph>
2023-10-05 15:06:14 +0800

    profile edit values

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -2,12 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ezmoney/models/buttonStyle.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'User.dart';
 import 'models/appstyle.dart';
 import 'models/birthdatefield.dart';
 import 'models/combobox.dart';
-import 'models/passwordtextfield.dart';
 import 'models/textfield.dart';
 import 'models/validators.dart';
 
@@ -62,6 +60,20 @@ class _SignUpState extends State<EditProfile> {
     }
   }
 
+  Future<Users> getUserData() async {
+    final user = FirebaseAuth.instance.currentUser!;
+    final userId = user.uid;
+
+    DocumentSnapshot snapshot =
+        await FirebaseFirestore.instance.collection('User').doc(userId).get();
+
+    if (snapshot.exists) {
+      return Users.fromJson(snapshot.data() as Map<String, dynamic>);
+    } else {
+      throw Exception("User data not found");
+    }
+  }
+
   TextEditingController fnameController = TextEditingController();
   TextEditingController lnameController = TextEditingController();
   TextEditingController birthdateController = TextEditingController();
@@ -95,164 +107,148 @@ class _SignUpState extends State<EditProfile> {
   @override
   void initState() {
     super.initState();
-    _isObscured = true;
-    _selectedGender = _genderList[0];
+
+    getUserData().then((userData) {
+      setState(() {
+        fnameController.text = userData.firstname;
+        lnameController.text = userData.lastname;
+        birthdateController.text = userData.birthdate;
+        _selectedGender = userData.gender;
+        addressController.text = userData.address;
+        phoneNumberController.text = userData.phoneNumber;
+        emailController.text = userData.email;
+        usernameController.text = userData.username;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryColor(1),
-      appBar: AppBar(
-        title: Text(
-          'Edit Profile',
-          style: fontTertiary(secondaryColor(1), FontWeight.bold),
-        ),
-        centerTitle: true,
-        surfaceTintColor: primaryColor(1),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 30,
-          ),
-          color: accentColor(1),
-        ),
+    return Theme(
+      data: Theme.of(context).copyWith(
+          dividerTheme:
+              const DividerThemeData(space: 0.0, color: Colors.transparent)),
+      child: Scaffold(
         backgroundColor: primaryColor(1),
-        elevation: 0,
-      ),
-      body: ListView(
-        children: [
-          Form(
-            key: _formKey,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  NormalTextField(
-                    txtController: fnameController,
-                    label: "First Name",
-                    validator:
-                        validateFirstName, // Now you can use the function directly
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  NormalTextField(
-                    txtController: lnameController,
-                    label: "Last Name",
-                    validator: validateLastName,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  BirthDateField(
-                    txtController: birthdateController,
-                    onTapMethod: () {
-                      _selectDate();
-                    },
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  ComboBox(
-                    label: "Gender",
-                    value: _selectedGender,
-                    itemList: _genderList.map((e) {
-                      return DropdownMenuItem(
-                        value: e,
-                        child: Text(e),
-                      );
-                    }).toList(),
-                    onChangeValue: (val) {
-                      setState(() {
-                        _selectedGender = val;
-                      });
-                    },
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  NormalTextField(
-                    txtController: addressController,
-                    label: "Address",
-                    validator: validateAddress,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  NormalTextField(
-                    txtController: phoneNumberController,
-                    label: "Phone Number",
-                    validator: validatePhoneNumber,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  NormalTextField(
-                    txtController: emailController,
-                    label: "Email",
-                    validator: validateEmail,
-                  ),
-                  NormalTextField(
-                    txtController: usernameController,
-                    label: "Username",
-                    validator: validateUsername,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  PasswordField(
-                    txtController: passwordController,
-                    label: "Password",
-                    validator: validatePassword,
-                    iconVal: _isObscured
-                        ? GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _isObscured = !_isObscured;
-                              });
-                            },
-                            child: const Icon(Icons.visibility),
-                          )
-                        : GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _isObscured = !_isObscured;
-                              });
-                            },
-                            child: const Icon(Icons.visibility_off),
-                          ),
-                    obscurevalue: _isObscured,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Button(
-                    btnLabel: "Save",
-                    onPressedMethod: () {
-                      if (_formKey.currentState!.validate()) {}
-                      updateUserProfile(
-                        firstName: fnameController.text,
-                        lastName: lnameController.text,
-                        birthdate: birthdateController.text,
-                        gender: _selectedGender!,
-                        address: addressController.text,
-                        phoneNumber: phoneNumberController.text,
-                        email: emailController.text,
-                        username: usernameController.text,
-                      );
-                      Navigator.pop(context);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                ],
+        appBar: AppBar(
+          title: Text(
+            'Edit Profile',
+            style: fontTertiary(secondaryColor(1), FontWeight.bold),
+          ),
+          centerTitle: true,
+          surfaceTintColor: primaryColor(1),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 30,
+            ),
+            color: accentColor(1),
+          ),
+          backgroundColor: primaryColor(1),
+          elevation: 0,
+        ),
+        body: ListView(
+          children: [
+            Form(
+              key: _formKey,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NormalTextField(
+                      txtController: fnameController,
+                      label: "First Name",
+                      validator:
+                          validateFirstName, // Now you can use the function directly
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    NormalTextField(
+                      txtController: lnameController,
+                      label: "Last Name",
+                      validator: validateLastName,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    BirthDateField(
+                      txtController: birthdateController,
+                      onTapMethod: () {
+                        _selectDate();
+                      },
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    ComboBox(
+                      label: "Gender",
+                      value: _selectedGender,
+                      itemList: _genderList.map((e) {
+                        return DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        );
+                      }).toList(),
+                      onChangeValue: (val) {
+                        setState(() {
+                          _selectedGender = val;
+                        });
+                      },
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    NormalTextField(
+                      txtController: addressController,
+                      label: "Address",
+                      validator: validateAddress,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    NormalTextField(
+                      txtController: phoneNumberController,
+                      label: "Phone Number",
+                      validator: validatePhoneNumber,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    NormalTextField(
+                      txtController: usernameController,
+                      label: "Username",
+                      validator: validateUsername,
+                    ),
+                  ],
+                ),
               ),
+            ),
+          ],
+        ),
+        persistentFooterButtons: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(bottom: 25, left: 10, right: 10),
+            child: Button(
+              btnLabel: "Save",
+              onPressedMethod: () {
+                if (_formKey.currentState!.validate()) {}
+                updateUserProfile(
+                  firstName: fnameController.text,
+                  lastName: lnameController.text,
+                  birthdate: birthdateController.text,
+                  gender: _selectedGender!,
+                  address: addressController.text,
+                  phoneNumber: phoneNumberController.text,
+                  email: emailController.text,
+                  username: usernameController.text,
+                );
+                Navigator.pop(context);
+              },
             ),
           ),
         ],
